be40d74cb49c9b8b039467c4bec88c50ebe269e2
Michał Nowak <nowik114@gmail.com>
2019-01-24 19:15:29 +0100

    First commit for glitch garden game

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+
+        private void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitAndLoad());
+        }
+    }
+
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
