f0125daf5c27b9b10be3dd20f21fe480883b569e
Michał Nowak <nowik114@gmail.com>
2019-01-30 21:26:16 +0100

    Dodanie canvasa ukonczenia poziomu

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour {
+
+    [SerializeField] GameObject winLabel;
+    int numberOfAttackers = 0;
+    bool levelTimerFinished = false;
+    [SerializeField] float waitToLoad = 4f;
+
+    private void Start()
+    {
+        winLabel.SetActive(false);
+    }
+
+    public void AttackerSpawned()
+    {
+        numberOfAttackers++;
+    }
+
+    public void AttackerKilled()
+    {
+        numberOfAttackers--;
+        if (numberOfAttackers <= 0 && levelTimerFinished)
+        {
+            StartCoroutine(HandleWinCondition());
+        }
+    }
+
+    IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(waitToLoad);
+        FindObjectOfType<LoadScene>().LoadNextScene();
+    }
+
+    public void LevelTimerFinished()
+    {
+        levelTimerFinished = true;
+        StopSpawners();
+    }
+
+    private void StopSpawners()
+    {
+        AttackerSpawner[] spawnerArray = FindObjectsOfType<AttackerSpawner>();
+        foreach (AttackerSpawner spawner in spawnerArray)
+        {
+            spawner.StopSpawning();
+        }
+    }
+
+}
